41136e6d5c2624bdf9ea55b4107087233fb8f877
cjc7@williams.edu Cao <cjc7@williams.edu>
2018-01-27 20:10:21 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,21 @@
-var ingredient1 = "bread"
-var ingredient2 = "mild cheese"
-var ingredient3 = "sharp cheese"
-var ingredient4 = "butter"
-var ingredient5 = "tomato"
-var ingredient6 = "garlic"
+function addElementToBeginningOfArray(array, element){
+  return [element, ...array]
+}
+
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  return array.unshift(element)
+}
+
+function addElementToEndOfArray(array, element){
+  return [...array,element]
+}
+
+function destructivelyAddElementToEndOfArray(array,element){
+  return array.push(element)
+}
+
+function accessElementInArray(array, index){
+  return array[index]
+}
+
